4f60f207d95b78ab84bb76ff0e92dc8265394b93
Mihaly24 <fairuzk20@gmail.com>
2025-10-20 12:53:20 +0700

    Menambahkan metode delete

--- index.js
+++ index.js
@@ -76,3 +76,14 @@ app.put('/api/users/:id', (req, res) => {
         }
     );
 });
+
+app.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.query('DELETE FROM users WHERE id = ?', [userId], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Database Error' });
+        }
+        res.json({ message: 'User deleted successfully' });
+    });
+});
