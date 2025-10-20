5566afde6748efaee593f8af1862ac1ece643483
Mihaly24 <fairuzk20@gmail.com>
2025-10-20 12:57:32 +0700

    Ganti nama table

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.put('/api/users/:id', (req, res) => {
 
 app.delete('/api/users/:id', (req, res) => {
     const userId = req.params.id;
-    db.query('DELETE FROM users WHERE id = ?', [userId], (err, results) => {
+    db.query('DELETE FROM mahasiswa WHERE id = ?', [userId], (err, results) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ message: 'Database Error' });
